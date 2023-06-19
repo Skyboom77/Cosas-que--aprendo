2dd1cb655619e418ef88880e75ea31ddf91f311e
MarcosSky <marja2783@gmail.com>
2023-06-19 13:11:21 -0300

    aprendo JS

---
+++ primero.js
@@ -0,0 +1,13 @@
+
+
+/* const contenedor=document.querySelector (".contenedor");
+const item = document.createElement("LI");
+const texDelItem= document.createTextNode("mi primer item de Js");
+item.appendChild(texDelItem);
+item.innerHTML =" Esta es otra forma de insertar un texto";*/
+
+//document .getElementById("que loco no?") .textContent="lo logre";//
+//<p id="que loco no?">lo logre</p>//
+
+let nombre = prompt (" soy marcos, y vos?");
+alert("hola" + nombre);
